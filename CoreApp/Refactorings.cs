aa2508efb2584b4cddf9a98cc3def611b73cc2d0
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-08-15 15:32:44 +0200

    Update.

---
+++ CoreApp/Refactorings.cs
@@ -0,0 +1,20 @@
+﻿namespace CoreApp;
+
+public class Refactorings
+{
+    private static void MoveMe(int intParameter)
+    {
+        
+    }
+
+    public static void UseMoveMe()
+    {
+        int a = 2;
+        MoveMe(a);
+    }
+}
+
+public class Refactorings2
+{
+    //private static void MoveMe(int a) { }
+}
---
+++ CoreApp/Undo.cs
@@ -0,0 +1,54 @@
+﻿
+
+using System.Collections.Immutable;
+
+namespace CoreApp;
+
+public class Undo
+{
+    private object _myObject;
+
+    public void Test(B parameter)
+    {
+    }
+
+    public void UseTest(A someString)
+    {
+        Test( /*sel start*/someString /*sel end*/);
+        Test(someString); // comment
+        Test(someString); //a
+        Test(someString); //comment
+
+        _myObject = someString;
+
+        NewMethod(someString);
+        Test(someString);
+
+        var list = new List<A>();
+
+        list.Add(new A());
+
+        var enumerable = list.Where(x => x.Name.Contains(""));
+
+        var a = enumerable.ToImmutableSortedSet();
+    }
+
+    private void NewMethod(object someString)
+    {
+        someString = (string)_myObject;
+    }
+}
+
+public class B
+{
+}
+
+public class A
+{
+    public string Name;
+
+    public static implicit operator B(A a)
+    {
+        return new B();
+    }
+}
