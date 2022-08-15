aa89d8ee12382475c03c8c489cb9f8eaef745819
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-08-15 15:53:44 +0200

    Update.

--- CoreApp/Refactorings.cs
+++ CoreApp/Refactorings.cs
@@ -4,11 +4,36 @@ public class Refactorings
 {
     private static void MoveMe(int intParameter)
     {
-        
     }
 
     public static void UseMoveMe()
     {
+        int a = 2;
+        int a1 = 2;
+        int a2 = 2;
+        int a3 = 2;
+        int a4 = 2;
+        int a5 = 2;
+        int a6 = 2;
+        int a7 = 2;
+        int a8 = 2;
+        int a9 = 2;
+        int a0 = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
+        int a = 2;
         int a = 2;
         MoveMe(a);
     }
