16e15eb60eaf1e80057279cf3ab15c4d3df2c898
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2022-08-17 11:17:56 +0200

    Update.

---
+++ CoreApp/AllTypesOfHints.cs
@@ -0,0 +1,65 @@
+﻿namespace CoreApp;
+
+public class AllTypesOfHints
+{
+    /// <summary>
+    /// </summary>
+    /// <param name="parameterMyInnerClass">This is xml doc</param>
+    /// <returns></returns>
+    public MyInnerClass2 ParameterNameHints(MyInnerClass? parameterMyInnerClass,
+        string                                            name,
+        int                                               age)
+    {
+        //return null;
+        // missing returns
+    }
+
+    public void MissingUsing()
+    {
+        using var stream = new StreamReader("");
+        // missing returns
+    }
+
+    public void UseHints()
+    {
+        // Parameter name hints
+        var someLocal = ParameterNameHints(
+            null,
+            "MyCoolName",
+            10);
+
+        // type name hints
+        var x = new
+        {
+        };
+
+        var y = new
+        {
+            p1 = "",
+            p2 = "",
+            p3 = ""
+        };
+
+        new List<int>().Where(x => x == 1);
+
+        new List<MyInnerClass>().Where(x =>
+            x.Name.Contains("a")
+                .ToString()
+                .Equals("")
+        );
+
+        var task = new Task(() => { });
+        var equals = task
+            .AsyncState
+            .Equals("");
+    }
+}
+
+public class MyInnerClass2
+{
+}
+
+public class MyInnerClass
+{
+    public string Name { get; set; }
+}
--- CoreApp/Completion.cs
+++ CoreApp/Completion.cs
@@ -4,6 +4,11 @@ public class Completion
 {
     public void Test()
     {
+        Test2_renamed("");
+    }
+
+    public void Test2_renamed(string sRenamed)
+    {
         
     }
 }
